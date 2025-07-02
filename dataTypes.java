1dfbfd9cec909847931801fbfdaed6f22ebb91c1
Shivansh139 <shivanshpandit2612@gmail.com>
2025-07-02 20:28:27 +0530

    new practice program

---
+++ dataTypes.java
@@ -0,0 +1,20 @@
+public class dataTypes {
+    public static void main(String[] args) {
+        byte b = 127;
+        short s = 32000;
+        int i = 100000;
+        long l = 5000000000L;
+        float f = 5.75f;
+        double d = 123.456;
+        char ch = 'A';
+        boolean bool = true;
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + ch);
+        System.out.println("boolean: " + bool);
+    }
+}
