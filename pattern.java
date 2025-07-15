8a68a68cda4340f3c137d1466486d324cae5f0d8
Shivansh139 <shivanshpandit2612@gmail.com>
2025-07-15 19:10:40 +0530

    star pattern

--- pattern.java
+++ pattern.java
@@ -1,7 +1,7 @@
 public class pattern {
     public static void main(String[] args) {
         for(int row = 0; row < 5; row++ ){
-            for(int col = 0; col< row; col++) {
+            for(int col = 0; col < row; col++) {
                 System.out.print("*");
             }
             System.out.println();
